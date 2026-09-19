dad0c8f9f721e00222857e41ef20c9f8f637d8eb
burnokshano-dot <burnokshano@gmail.com>
2026-09-19 10:20:01 +0800

    Hello vnce

--- student-management-portal/src/App.jsx
+++ student-management-portal/src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Students from "./pages/Students";
+import Search from "./pages/Search";
 import AddStudent from "./pages/AddStudent";
 import StudentDetails from "./pages/StudentDetails";
 
@@ -7,6 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/students/add" element={<AddStudent />} />
         <Route path="/students/:id" element={<StudentDetails />} />
       </Routes>
